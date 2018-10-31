83edd8f9aea12c595acca70b4a6d8dfede411516
Yuhan Liu <yl5504@nyu.edu>
2018-10-31 13:07:24 -0400

    add some comments

--- project1/project1/sketch.js
+++ project1/project1/sketch.js
@@ -1,45 +1,53 @@
-var stars= []; var starstwo = []; var starsthree = [];
-var x,y; var i=0; 
+var stars= []; var starstwo = []; 
+var z = 1;
+var bg;
+
+var paths= [];
+var current; var previous;
+var painting = false;
+
 
 function setup(){
   createCanvas(windowWidth,windowHeight);
   background(0);
   colorMode(HSB);
 
+  bg = loadImage('data/background.png')
+
+  current = createVector(0,0);
+  previous = createVector(0,0);
+
+  //First screen full of balls needed to be wiped out
   for(var n = 0; n < 500; n++){   
-    var col=n%250;    
-    var r= sin(n) * 8;
-    var x = random(0,width); 
-    var y = random(0,height);
+    var col=n%250;    //color of each ball
+    var r= sin(n) * 12;  //radius of each ball
+    var x = random(0,width); //x position of each ball
+    var y = random(0,height); //y position of each ball
     stars[n] = new Star(x,y,col,r);
   }
 
-  for(var n = 0; n < 500; n++){   
+  //Second screen of spiraling balls
+  for(var n = 0; n < 1000; n++){   
     var col=n%250;    
-    var r= sin(n) * 8;
+    var r= sin(n) * 12 / z;
     var x = random(-width/2,width/2); 
     var y = random(-height/2,height/2);
     starstwo[n] = new Star(x,y,col,r);
   }
-
-  //havent use this part yet
-  for(var n = 0; n < 500; n++){   
-    var col=n%250;    
-    var r= sin(n) * 8;
-    var x = random(0,width); 
-    var y = random(0,height);
-    starsthree[n] = new Star(x,y,col,r);
-  }
   
 }
+
+
 
 function draw(){
   background(0);
   var mouse = createVector(mouseX,mouseY)
   var flag = true;
 
+
   for(var n = 0; n<stars.length; n++){
     var s = stars[n];
+    s.col = n * frameCount /300  //color changes with time
     s.show();
     s.scatter(mouse);    //move your mouse to erase every dots!
     s.update();    
@@ -53,51 +61,72 @@ function draw(){
   // erase every dots, and then new thing will show!!
   if (flag == true){
 
-    for(var n = 0; n<500; n++){
+    image(bg,0,0,width,height)
+
+
+    if (painting){
+      current = mouse
+      paths[paths.length-1].addnewline(current);
+      previous = current
+    }
+    for(var i = 0; i<paths.length;i++){
+      paths[i].show();
+    }
+
+    for(var n = 0; n<starstwo.length; n++){
       push()
       translate(width/2,height/2);
       var rot = map(mouseX, 0, width,-1,1);
       rotate(rot * frameCount/200)        
       var t = starstwo[n];
       t.show();
-      let target = createVector((n * sin(n) ), (n * cos(n) ));
+      let target = createVector(n/z*sin(n), n/z*cos(n))
       t.arrive(target);
       t.update();
       pop()
 
       //press mouse to attract the dots
-      if(mouseIsPressed){
-        var mouse = createVector(mouseX,mouseY)
-        var center = createVector(width/2,height/2)
-        var tar = p5.Vector.sub(center,mouse)
-        t.arrive(tar);
-      }
+      // if(mouseIsPressed){
+      //   var mouse = createVector(mouseX,mouseY)
+      //   var center = createVector(width/2,height/2)
+      //   var tar = p5.Vector.sub(center,mouse)
+      //   t.arrive(tar);
+      // }
+
+    
 
       // press key space to see random dots flying
       if(keyIsPressed && key ==' '){
         t.pos = createVector(random(-width/2,width/2),random(-height/2,height/2))
       }
      
+    }
 
+    if (keyIsPressed) {
+        if (key == 'a') z -= 2;
+        else if (key == 's') z += 2;
+        else if (key == 'd') z *= -1;
+        else if (key == 'r') z = 1;
+
+        for (var n = 0;n<starstwo.length;n++){
+          push()
+          translate(width/2,height/2)
+          var t = starstwo[n];
+          var r= sin(n) * 12 ;
+          var x = n*sin(n)/z; 
+          var y = n*cos(n)/z;
+          t.show();
+          pop()
+        }
     }
 
     //a pattern orbiting like planets
     planet();
-
-
-  }
-
-
-  //this part is useless now..
-  if (keyIsPressed && key ==' '){
-    for(var n = 0; n<starsthree.length;n++){
-      var h = starsthree[n];
-      h.show();
-    }
   }
 
 
 }
+
 
 
 function planet(){
@@ -117,8 +146,6 @@ function planet(){
     x = x +0.5;
   }
 }
-
-
 
 
 
@@ -155,13 +182,12 @@ class Star{
   
   applyForce(f){
     this.acc.add(f);
-    //this.acc.setMag(2.0);
   }
   
   update(){
-    this.vel.add(this.acc);
+    this.vel.add(this.acc); // Update velocity
     this.pos.add(this.vel);
-    this.acc.mult(0);
+    this.acc.mult(0);   // Reset accelerationelertion to 0 each cycle
   }
 
 
@@ -175,15 +201,55 @@ class Star{
     } else {
       desired.setMag(this.maxspeed);
     }
+    // Steering = Desired minus Velocity
     var steer = p5.Vector.sub(desired, this.vel);
     steer.limit(this.maxforce); // Limit to maximum steering force
     this.applyForce(steer);
   }
+}
 
 
-  
-  
 
-  
+
+class Path{
+  constructor(){
+    this.particles = []
+  }
+  show(){
+    for(var i = 0; i<this.particles.length;i++){
+      this.particles[i].show(this.particles[i+1])
+      this.particles[i].update();
+    }
+  }
+  addnewline(position){
+    this.particles.push(new Particle(position))
+  }
+}
+
+class Particle{
+  constructor(position){
+    this.position = createVector(position.x,position.y)
+    this.lifespan = 200;
+  }
+  show(another){
+    stroke(255,this.lifespan)
+    fill(255,this.lifespan)
+    ellipse(this.position.x,this.position.y,10,10)
+    if (another){
+      line(this.position.x,this.position.y,another.position.x,another.position.y)
+    }
+  }
+  update(){
+    this.lifespan = this.lifespan -1
+  }
+}
+
+function mousePressed(){
+  painting = true;
+  paths.push(new Path());
+}
+
+function mouseReleased(){
+  painting = false;
 }
 
